9ac8d58de470a6784fc7fccea9a03001cc9fd267
molecule03 <laveshsalaskar619@gmail.com>
2023-07-07 03:21:49 +0530

    Updated Code

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         var btnDatePicker : Button = findViewById(R.id.btnDatePicker)
 
         btnDatePicker.setOnClickListener {view->
